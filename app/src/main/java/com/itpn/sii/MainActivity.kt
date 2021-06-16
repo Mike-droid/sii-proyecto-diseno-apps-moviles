b9c09d57244d78a1d74faf74419ccad9edf35925
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2021-06-16 13:06:36 -0500

    Segunda pantalla lograda

--- app/src/main/java/com/itpn/sii/MainActivity.kt
+++ app/src/main/java/com/itpn/sii/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.itpn.sii
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun abrirHome(v: View){
+        val intentoHome: Intent = Intent(this, Home::class.java)
+        startActivity(intentoHome)
     }
 }
