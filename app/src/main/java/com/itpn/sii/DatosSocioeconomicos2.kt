7e6e3da2126f03e47a4e1df6f758d632bd74f408
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2021-06-20 16:29:16 -0500

    Pantalla DS2 creada

--- app/src/main/java/com/itpn/sii/DatosSocioeconomicos2.kt
+++ app/src/main/java/com/itpn/sii/DatosSocioeconomicos2.kt
@@ -1,11 +1,23 @@
 package com.itpn.sii
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class DatosSocioeconomicos2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_datos_socioeconomicos2)
+    }
+
+    fun abrirHome(v: View) {
+        val Home: Intent = Intent(this, Home::class.java)
+        startActivity(Home)
+    }
+
+    fun abrirDatosSocioeconomicos3(v: View) {
+        val ds3: Intent = Intent(this,DatosSocioeconomicos3::class.java)
+        startActivity(ds3)
     }
 }
---
+++ app/src/main/java/com/itpn/sii/DatosSocioeconomicos3.kt
@@ -0,0 +1,11 @@
+package com.itpn.sii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatosSocioeconomicos3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datos_socioeconomicos3)
+    }
+}
